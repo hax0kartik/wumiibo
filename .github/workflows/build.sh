2521d203ebae4feb88ffc795e26e6c9363c70f03
hax0kartik <pokemonemerald0@gmail.com>
2020-09-02 16:03:46 +0530

    Install ctrtool

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -8,12 +8,13 @@ git clone https://github.com/smealum/ctrulib.git
 cd ctrulib/libctru
 make install release -j
 
-# Install makerom
+# Install makerom and ctrtool
 cd /build_dir
 export ctr_ver=16
 curl -LJO https://github.com/jakcron/Project_CTR/releases/download/v0."$ctr_ver"/makerom_0"$ctr_ver"_ctrtool.zip
 7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/makerom
-chmod +x /usr/bin/makerom
+7z e makerom_0"$ctr_ver"_ctrtool.zip -o/usr/bin Ubuntu/ctrtool
+chmod +x /usr/bin/makerom && chmod +x /usr/bin/ctrtool
 
 #Build wumiibo
 make clean && make -j2
